f0fdbcca5821c053489f6a98e5680c93c322c44e
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-08-30 18:08:59 +0300

    done with lab27

--- README.md
+++ README.md
@@ -16,3 +16,20 @@ allow users to type in details about a new task, specifically a title and a body
 should just be an image with a back button; it needs no functionality.
 
 ![All Tasks](screenshots/allTaskesPage.PNG)
+### Lab: 27 - Data in TaskMaster
+1. Homepage
+
+The main page should be modified to contain three different buttons with hardcoded task titles. When a user taps one of the titles, it should go to the Task Detail page, and the title at the top of the page should match the task title that was tapped on the previous page.
+
+The homepage should also contain a button to visit the Settings page, and once the user has entered their username, it should display “{username}’s tasks” above the three task buttons.
+
+![Home Page](screenshots/homePagelab27.PNG)
+
+4. Detail page
+Create a Task Detail page. It should have a title at the top of the page, and a Lorem Ipsum description.
+
+![Detail page](screenshots/detailPage.PNG)
+
+5. Settings page
+Create a Settings page. It should allow users to enter their username and hit save.
+![Settings page](screenshots/settingNew.PNG)
--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,37 +35,99 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(transferToAddTask);
             }
         });
+
+        Button labButton=findViewById(R.id.finishLab27ButtonId);
+        labButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent transferToDetailsTask=new Intent(MainActivity.this,TaskDetails.class);
+                String taskValue=labButton.getText().toString();
+                transferToDetailsTask.putExtra("taskTitle",taskValue);
+                startActivity(transferToDetailsTask);
+
+
+            }
+        });
+
+        Button codeButton=findViewById(R.id.finishCode27ButtonId);
+        codeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent transferToDetailsTask=new Intent(MainActivity.this,TaskDetails.class);
+                String taskValue=codeButton.getText().toString();
+                transferToDetailsTask.putExtra("taskTitle",taskValue);
+                startActivity(transferToDetailsTask);
+
+
+            }
+        });
+
+        Button readButton=findViewById(R.id.readButton);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent transferToDetailsTask=new Intent(MainActivity.this,TaskDetails.class);
+                String taskValue=readButton.getText().toString();
+                transferToDetailsTask.putExtra("taskTitle",taskValue);
+                startActivity(transferToDetailsTask);
+
+
+            }
+        });
+        Button goToSettingButton=findViewById(R.id.goToSettingId);
+        goToSettingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent transferToDetailsTask=new Intent(MainActivity.this,SettingsPage.class);
+                startActivity(transferToDetailsTask);
+            }
+        });
+
     }
 //
     @Override
     protected void onStart() {
         super.onStart();
-//        setContentView(R.layout.activity_main);
-//        Toast.makeText(getApplicationContext(),"from onStart",Toast.LENGTH_LONG).show();
+
+        Toast.makeText(getApplicationContext(),"from onStart",Toast.LENGTH_LONG).show();
+
+        //Show user name in Home page
+        SharedPreferences sharedPreferences= PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String userNameString=sharedPreferences.getString("userName","user");
+        TextView userNameView=findViewById(R.id.viewUserNameId);
+        userNameView.setText(userNameString);
+
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Toast.makeText(getApplicationContext(),"from onStop",Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Toast.makeText(getApplicationContext(),"from onDestroy",Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Toast.makeText(getApplicationContext(),"from onPause",Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Toast.makeText(getApplicationContext(),"from onResume",Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Toast.makeText(getApplicationContext(),"from onRestart",Toast.LENGTH_LONG).show();
+
     }
 }
