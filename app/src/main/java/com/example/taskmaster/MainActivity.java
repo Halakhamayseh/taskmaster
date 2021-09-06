f2a341a225b52a280ec424eb711ac5283383b841
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-09-06 22:37:26 +0300

    done with inseret

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -9,10 +9,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.TaskMaster;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +30,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+////////////////////////////////
+        try {
+            // Add these lines to add the AWSApiPlugin plugins
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.configure(getApplicationContext());
+
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
+        }
+        /////////////////////////////////////////////////
 
         Button addTaskButton=findViewById(R.id.buttonADDTASK);
         addTaskButton.setOnClickListener(new View.OnClickListener() {
@@ -128,6 +146,17 @@ public class MainActivity extends AppCompatActivity {
         allTaskRecuclerView.setLayoutManager(new LinearLayoutManager(this));
         //set Adapter and pass to it the object
         allTaskRecuclerView.setAdapter(new TaskAdapter(task));
+
+        // show from amplify from log
+//        Amplify.API.query(
+//                ModelQuery.list(TaskMaster.class),
+//                response -> {
+//                    for (TaskMaster taskMaster : response.getData()) {
+//                        Log.i("MyAmplifyApp", taskMaster.getTaskTitle());
+//                    }
+//                },
+//                error -> Log.e("MyAmplifyApp", "Query failure", error)
+//        );
 
     }
 
